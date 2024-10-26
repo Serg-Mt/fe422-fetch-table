cd3f7f3a0b70d9f5df9759af2ea17880e995bb34
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2024-10-26 14:01:32 +0400

    в конце занятия

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,19 @@
 import Head from "next/head";
+import { Fetcher } from '../components/Fetcher';
+import { ObjTable } from '../components/ObjTable';
+import { useState } from 'react';
 
 export default function Home() {
+  // [user,setUser] = useState(null);
   return <>
     <Head>
       <title>Create Next App</title>
     </Head>
-    <h1>Hello</h1>
+    <Fetcher url="https://jsonplaceholder.typicode.com/users" >
+     
+    </Fetcher>
+{/* 
+    <Fetcher url={''} callback={data=>setUser(data)}/>
+    {user && <User user={user/>}  */}
   </>
 }
